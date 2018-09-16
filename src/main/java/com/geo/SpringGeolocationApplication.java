48e0fd01ed6ad4cde076de52066ae55f8c6f7069
Niranjan <nirpanig@publicisgroupe.net>
2018-09-16 12:23:29 +0530

    first commit

---
+++ src/main/java/com/geo/SpringGeolocationApplication.java
@@ -0,0 +1,12 @@
+package com.geo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGeolocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGeolocationApplication.class, args);
+	}
+}
